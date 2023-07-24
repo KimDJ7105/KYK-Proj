ff6e2b167592fbceb88675ef2489bfaea45cca37
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-07-24 13:14:32 +0900

    asio 서버, 클라이언트 실행 가능한 상태로 다시 push

--- 김동재(서버)/Asio server 연습/server/ASIO_GAME_SERVER/NPC.lua
+++ 김동재(서버)/Asio server 연습/server/ASIO_GAME_SERVER/NPC.lua
@@ -4,10 +4,10 @@ function set_uid(x)
 end
 
 function event_player_move(player)
-	player_x = API_get_x(player);
-	player_y = API_get_y(player);
-	my_x = API_get_x(myid);
-	my_y = API_get_y(myid);
+	player_x = API_get_x_p(player);
+	player_y = API_get_y_p(player);
+	my_x = API_get_x_n(myid);
+	my_y = API_get_y_n(myid);
 	if (player_x == my_x) then
 		if (player_y == my_y) then
 		API_SendMessage(myid, player, "HELLO");
