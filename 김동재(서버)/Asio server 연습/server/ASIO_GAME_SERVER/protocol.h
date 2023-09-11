9deb41fed6800d550b3266c70535108538463ae4
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-09-11 18:18:16 +0900

    Update protocol.h
    이동 패킷에 이동 시간을 추가하도록 변경

--- 김동재(서버)/Asio server 연습/server/ASIO_GAME_SERVER/protocol.h
+++ 김동재(서버)/Asio server 연습/server/ASIO_GAME_SERVER/protocol.h
@@ -20,11 +20,13 @@
 
 #define MAX_STR_SIZE  100
 
+#define CS_MOVE
 #define CS_UP    1
 #define CS_DOWN  2
 #define CS_LEFT  3
 #define CS_RIGHT    4
 #define CS_CHAT		5
+#define CS_LOGOUT 6
 
 #define SC_POS           1
 #define SC_PUT_PLAYER    2
@@ -38,21 +40,25 @@
 struct cs_packet_up {
 	BYTE size;
 	BYTE type;
+	unsigned int move_time;
 };
 
 struct cs_packet_down {
 	BYTE size;
 	BYTE type;
+	unsigned int move_time;
 };
 
 struct cs_packet_left {
 	BYTE size;
 	BYTE type;
+	unsigned int move_time;
 };
 
 struct cs_packet_right {
 	BYTE size;
 	BYTE type;
+	unsigned int move_time;
 };
 
 struct cs_packet_chat {
@@ -61,12 +67,18 @@ struct cs_packet_chat {
 	WCHAR message[MAX_STR_SIZE];
 };
 
+struct cs_packet_logout {
+	BYTE size;
+	BYTE type;
+};
+
 struct sc_packet_pos {
 	BYTE size;
 	BYTE type;
 	WORD id;
 	WORD x;
 	WORD y;
+	unsigned int move_time;
 };
 
 struct sc_packet_put_player {
